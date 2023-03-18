5e75de150d1e9bcc4bcb8897c48213596b1f3e10
Kate <3837437+katesroad@users.noreply.github.com>
2023-03-18 15:07:10 -0400

    change fodler structure

--- code/JavaScript/httpClient.ts
+++ code/JavaScript/httpClient.ts
@@ -1,3 +1,8 @@
+/**
+ * Author: Kate, Zhang
+ * Date: 03/18/2023
+ */
+
 export default class HttpClient {
   private readonly baseUri: string;
   private readonly options: HttpClientOptions;
@@ -7,16 +12,14 @@ export default class HttpClient {
     this.options = options ?? {};
   }
 
-  private getRequestBody(body: unknown): { body: BodyInit | null | undefined } {
-    if (body === undefined) {
+  private getRequestBody(body: unknown): { body: RequestInit["body"] } {
+    if (body instanceof FormData) {
       return { body };
-    } else if (body instanceof FormData) {
-      return { body } as { body: BodyInit };
-    } else if (typeof body === "object") {
+    } else if (typeof body === "object" && body !== null ) {
       return { body: JSON.stringify(body) };
     }
 
-    return { body } as { body: BodyInit };
+    return { body } as { body: RequestInit["body"] };
   }
 
   private async request<T>(conf: RequestConf): Promise<T | undefined> {
@@ -24,15 +27,19 @@ export default class HttpClient {
 
     const search = query ? `?${new URLSearchParams(query).toString()}` : "";
     const url = `${this.baseUri}/${apiPath}${search}`;
+    const ignoreHeader = data instanceof FormData;
 
     const reqestInit = {
       ...this.options,
       method: method.toUpperCase(),
       ...(data !== undefined && this.getRequestBody(data)),
-      headers: {
-        ...this.options.headers,
-        ...conf.headers,
-      },
+      // to upload multiplepart file: https://stackoverflow.com/questions/36067767/how-do-i-upload-a-file-with-the-js-fetch-api
+      ...(!ignoreHeader && {
+        headers: {
+          ...this.options.headers,
+          ...conf.headers,
+        },
+      }),
     };
 
     const response = await fetch(url, reqestInit);
